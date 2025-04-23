fe5daec92f068998c8f0ff88814936dabda4f26d
Gazal <gtech5277@gmail.com>
2025-04-23 13:26:53 +0530

    corrected version errors

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cocktails-app-start'),
+      dir: require('path').join(__dirname, './coverage/cocktail-web-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
